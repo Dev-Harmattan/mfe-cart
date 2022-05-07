2782446bbcbc295b0328a73f68cceb6da152365d
Sodiq Olalekan <ganiyusodiq059@gmail.com>
2022-05-07 14:17:14 -0600

    sub app excution contex

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -1,4 +1,17 @@
 import faker from 'faker';
-const myCart = `<div>You have ${faker.random.number()} items in you cart</div>`;
 
-document.querySelector('#dev-cart').innerHTML = myCart;
+const mountCart = (el) => {
+  const myCart = `<div>You have ${faker.random.number()} items in you cart</div>`;
+  el.innerHTML = myCart;
+}
+
+
+if(process.env.NODE_ENV === 'development') {
+  const el = document.querySelector('#dev-cart');
+  if(el){
+    mountCart(el);
+  }
+}
+
+
+export { mountCart };
